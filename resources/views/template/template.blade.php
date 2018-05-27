5ba339e5b3eb3750253a992609ef635ba706779e
Денис <dkochanovskiy@gmail.com>
2018-05-28 00:35:58 +0300

    шапка личного кабинета

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -12,97 +12,104 @@
 <body>
     <header>
         <div class="container">
-            <a href="/">
-                <div class="header-logo"></div>
-            </a>
+            <div class="header-logo-wrap">
+                <a href="/">
+                    <div class="header-logo"></div>
+                </a>
+            </div>
             @guest
-            @else
-                <ul class="header-links">
-                    <li><a href="/"><span class="glyphicon glyphicon-envelope"></span>Сообщения</a></li>
-                    <li><a href="/"><span class="glyphicon glyphicon-star"></span>Избранное</a></li>
-                </ul>
-            @endelse
             <div class="header-auth">
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        @guest
                         <li><a href="javascript:PopupLoginShow()">Войти</a></li>
                         <li><a href="javascript:PopupRegistrationShow()">Регистрация</a></li>
-                    @else
-                    </ul>
-                        <ul class="nav navbar-nav personal-area-navbar-right">
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->email }} <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('my-announcements') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('my-announcements-form').submit();">
-                                            Мои объявления
-                                        </a>
-                                        <form id="my-announcements-form" action="{{ route('my-announcements') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('profile') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('profile-form').submit();">
-                                            Профиль
-                                        </a>
-                                        <form id="profile-form" action="{{ route('profile') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('purse') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('purse-form').submit();">
-                                            Кошелек
-                                        </a>
-                                        <form id="purse-form" action="{{ route('purse') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('amend-contract') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('amend-contract-form').submit();">
-                                            Изменить договор
-                                        </a>
-                                        <form id="amend-contract-form" action="{{ route('amend-contract') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('ad-delivery') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('ad-delivery-form').submit();">
-                                            Подать объявления
-                                        </a>
-                                        <form id="ad-delivery-form" action="{{ route('ad-delivery') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Выйти
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-
-                                </ul>
-                            </li>
-                            @endguest
                     </ul>
                 </div>
             </div>
+            @else
+                <div class="header-registered">
+                    <div class="header-registered-item-wrap">
+                        <div class="header-registered-item">
+                            <a href="/"><span class="glyphicon glyphicon-envelope"></span>&#160;Сообщения</a>
+                        </div>
+                        <div class="header-registered-item">
+                            <a href="/"><span class="glyphicon glyphicon-star"></span>&#160;Избранное</a>
+                        </div>
+                        <div class="header-registered-item">
+                            <ul>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                                        {{ Auth::user()->email }} <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href="{{ route('my-announcements') }}"
+                                               onclick="event.preventDefault();
+                                         document.getElementById('my-announcements-form').submit();">
+                                                Мои объявления
+                                            </a>
+                                            <form id="my-announcements-form" action="{{ route('my-announcements') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('profile') }}"
+                                               onclick="event.preventDefault();
+                                         document.getElementById('profile-form').submit();">
+                                                Профиль
+                                            </a>
+                                            <form id="profile-form" action="{{ route('profile') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('purse') }}"
+                                               onclick="event.preventDefault();
+                                         document.getElementById('purse-form').submit();">
+                                                Кошелек
+                                            </a>
+                                            <form id="purse-form" action="{{ route('purse') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('amend-contract') }}"
+                                               onclick="event.preventDefault();
+                                         document.getElementById('amend-contract-form').submit();">
+                                                Изменить договор
+                                            </a>
+                                            <form id="amend-contract-form" action="{{ route('amend-contract') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('ad-delivery') }}"
+                                               onclick="event.preventDefault();
+                                         document.getElementById('ad-delivery-form').submit();">
+                                                Подать объявления
+                                            </a>
+                                            <form id="ad-delivery-form" action="{{ route('ad-delivery') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                                Выйти
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                @endguest
         </div>
     </header>
     <section>
