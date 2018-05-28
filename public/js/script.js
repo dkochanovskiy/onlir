5c9bcdde1f6b0b04471b0ae71010d353144e3e2a
Денис <dkochanovskiy@gmail.com>
2018-05-29 01:16:43 +0300

    новый пароль

--- public/js/script.js
+++ public/js/script.js
@@ -10,6 +10,14 @@ function PopupRegistrationShow(){
 function PopupRegistrationHide(){
     $("#popup-registration").css('visibility', 'hidden');
 }
+function EditPhoneForm(){
+    $("#show-phone").css('visibility', 'hidden');
+    $("#edit-phone").css('visibility', 'visible');
+}
+function ShowPhoneForm(){
+    $("#edit-phone").css('visibility', 'hidden');
+    $("#show-phone").css('visibility', 'visible');
+}
 $(document).mouseup(function (e) {
     var container = $(".panel-body");
     if (container.has(e.target).length === 0){
--- resources/views/personal-area/settings.blade.php
+++ resources/views/personal-area/settings.blade.php
@@ -4,13 +4,40 @@
     <div class="container">
         @include('include.navigation-bar')
         <div class="container-content">
-            <h4>Телефон</h4>
-            <div class="edit-phone">
-                <form class="form-horizontal" role="form" id="edit-phone-form">
-                    <div class="edit-phone-input">
-                        <input type="text" class="form-control" id="phone">
+            <div class="phone-wrapper">
+                <div class="phone">Телефон:</div>
+                <div class="show-phone" id="show-phone">
+                    8(962) 594-3880&#160;
+                    <a href="javascript:EditPhoneForm()"><span class="glyphicon glyphicon-pencil"></span></a>
+                </div>
+                <div class="edit-phone" id="edit-phone">
+                    <form class="form-horizontal" role="form" id="edit-phone-form">
+                        <div class="edit-phone-input">
+                            <input type="text" class="form-control" id="phone" value="8888">
+                        </div>
+                        <button href="javascript:ShowPhoneForm()" type="submit" class="btn btn-primary">Сохранить</button>
+                    </form>
+                </div>
+            </div>
+            <div class="new-password">
+                <form class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <label for="inputPassword1" class="control-label">Введите новый пароль:</label>
+                        <div>
+                            <input type="password" class="form-control" id="inputPassword1">
+                        </div>
                     </div>
-                    <button type="submit" class="btn btn-primary">Сохранить</button>
+                    <div class="form-group">
+                        <label for="inputPassword2" class="control-label">Еще раз:</label>
+                        <div>
+                            <input type="password" class="form-control" id="inputPassword2">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-primary">Изменить</button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
