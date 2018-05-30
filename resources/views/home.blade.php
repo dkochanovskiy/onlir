bef76fa9cebfed709a6c3ce30f3b1a27fee4490b
Денис <dkochanovskiy@gmail.com>
2018-05-30 16:01:46 +0300

    объявления

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,23 @@
 <div class="container">
     @include('include.navigation-bar')
     <div class="container-content">
-        <div class="container-content-item active" id="active">
-            <h3>
-                Активные (0)
-            </h3>
-        </div>
-        <div class="container-content-item" id="completed">
-            <h3>
-                Завершенные (2)
-            </h3>
-        </div>
-        <div class="container-content-content">
-            Активных объявлений нет
-        </div>
+        <a href="">
+            <div class="container-content-item active" id="active">
+                <h3>
+                    Активные (0)
+                </h3>
+            </div>
+        </a>
+        <a href="">
+            <div class="container-content-item" id="completed">
+                <h3>
+                    Завершенные (2)
+                </h3>
+            </div>
+        </a>
+        {{--<div class="container-content-content">--}}
+            {{--Активных объявлений нет--}}
+        {{--</div>--}}
     </div>
 </div>
 @endsection
