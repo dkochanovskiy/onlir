790a656dd2af070f0348f2c0022dd620edd66b77
Денис <dkochanovskiy@gmail.com>
2018-05-31 16:24:06 +0300

    logo

--- resources/views/include/popup/popup-login.blade.php
+++ resources/views/include/popup/popup-login.blade.php
@@ -41,7 +41,7 @@
                             <div class="form-group">
                                 <div class="col-md-8 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Войти
+                                        <span class="glyphicon glyphicon-log-in"></span> Войти
                                     </button>
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         Забыли пароль?
--- resources/views/include/search-form.blade.php
+++ resources/views/include/search-form.blade.php
@@ -19,5 +19,9 @@
     <input type="text" value="{{old('max_price')}}" class="form-control" name="max_price" id="max_price" placeholder="до">
     <input type="text" value="{{old('city')}}" class="form-control" name="city" id="city" placeholder="Город">
     {{--<input type="submit" class="btn btn-default" value="Найти" />--}}
-    <a href="/search"><div class="btn btn-default">Найти</div></a>
+    <a href="/search">
+        <button class="btn btn-primary">
+            <span class="glyphicon glyphicon-search"></span> Поиск
+        </button>
+    </a>
 </form>
